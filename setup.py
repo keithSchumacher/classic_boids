707adec728221023e76a92b6e374d9f22711b312
keithschumacher <keithschumacher.daily@gmail.com>
2025-02-11 22:50:25 -0800

    add python version to setup

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="classic_boids",
     version="0.1.0",
+    python_requires=">=3.12",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
